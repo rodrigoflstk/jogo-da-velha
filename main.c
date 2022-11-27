d60b83d6ca554ca9f6217f349c7fa4fd826fd0dd
rodrigo <jrodrigo0592@gmail.com>
2022-11-27 15:53:43 -0300

    Adicionando estruturação da matriz

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char p1[50], p2[50];
+char matriz_jogo[3][3];
+
+void estruturarJogo() {
+    int lin, col;
+    for (lin < 0; lin = 3; lin++) {
+        for(col < 0; col = 3; col++) {
+            matriz_jogo[lin][col] = " ";
+        }
+    }
+}
 
 int main(int argc, char const *argv[])
 {
-    printf("Commit test");
+
     return 0;
 }
+
+
